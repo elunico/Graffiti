c9049424565ff676bce2d2978db74d3988aeaf6c
Tom <elunico@users.noreply.github.com>
2022-12-31 12:45:25 -0500

    Initial Commit

---
+++ Graffiti/GraffitiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraffitiApp.swift
+//  Graffiti
+//
+//  Created by Thomas Povinelli on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct GraffitiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
